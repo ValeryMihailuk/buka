fc376c7030a5b25b7f1e02d4473152b139f73ece
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-03-29 20:56:42 +0300

    refactor all classes

--- src/test/java/by/itacademy/valerymichailuk/buka/TestAPI.java
+++ src/test/java/by/itacademy/valerymichailuk/buka/TestAPI.java
@@ -12,8 +12,7 @@ import static io.restassured.RestAssured.*;
 public class TestAPI {
     @Test
     @DisplayName("Post422BadLoginPwd")
-    public void testPost() {
-        Map<String, Object> map = new HashMap<String, Object>();
+    public void testPost422() {
         JSONObject request = new JSONObject();
         request.put("login", "err");
         request.put("pwd", "err2");
@@ -27,9 +26,22 @@ public class TestAPI {
     }
 
     @Test
-    @DisplayName("Get200Login")
-    public void testGet() {
-        Map<String, Object> map = new HashMap<String, Object>();
+    @DisplayName("Post403BadPwd")
+    public void testPost403() {
+        JSONObject request = new JSONObject();
+        request.put("login", User.LOGIN_TEXT);
+        request.put("pwd", "err");
+        baseURI = "https://shop.buka.ru/api";
+        given().
+                body(request.toJSONString()).
+                when().
+                post("/f/v2/auth/login").
+                then().statusCode(403)
+                .log().all();
+    }
+    @Test
+    @DisplayName("Post200Authorization")
+    public void testPost200() {
         JSONObject request = new JSONObject();
         request.put("login", User.LOGIN_TEXT);
         request.put("pwd", User.PASSWORD_TEXT);
