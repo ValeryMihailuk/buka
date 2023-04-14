f1d33fdec7f88dc9b94fa93e3732de19af9d25f4
ValeryMihailuk <valery.mihkailuk@gmail.com>
2023-04-14 14:09:28 +0300

    refactor readme file

--- src/main/java/by/itacademy/valerymichailuk/buka/pages/Pages.java
+++ src/main/java/by/itacademy/valerymichailuk/buka/pages/Pages.java
@@ -3,6 +3,7 @@ package by.itacademy.valerymichailuk.buka.pages;
 
 public class Pages {
     public static final String URL = "https://www.shop.buka.ru/";
+    public static final String API_URL = "https://shop.buka.ru/api";
     public static final String ACCOUNT = "//div[1]/div[1]/div/a/img[@alt='authorization']";
     public static final String ACCOUNT_AFTER_AUTHORIZATION = "//*[@src='/media/new_img/ico-user.png']";
     public static final String BUTTON_INPUT_AUTHORIZATION = "//*[@class='btn-red width-100']";
@@ -10,7 +11,6 @@ public class Pages {
     public static final String PASSWORD_INPUT = "//*[@name='pwd']";
     public static final String ERROR = "//*[@class='error-login']/ul/li";
     public static final String CHECK_AUTHORIZATION = "//*[@class='h2']";
-    public static final String USER_ACCOUNT_OUTPUT = "(//*[@href='/logout'])[1]";
     public static final String INPUT_SEARCHING_TEXT = "//*[@name='search']";
     public static final String BUTTON_SEARCH = "//*[@id='showSearchResults']";
     public static final String ADD_IN_CARD_FOUNDING_GAME = "//*[@class='buy-button buy-button_width_full js-buy-btn']";
@@ -18,4 +18,5 @@ public class Pages {
     public static final String SEE_PRODUCT_IN_CARD = "//*[@alt='shopping cart']";
     public static final String PUT_PRODUCT_IN_CARD = "//*[@class='basket-tovar-close del']";
     public static final String CHECK_PRODUCT_IN_CARD = "//div[8]/div[2][@class=\"nav-popup-body\"]";
+
 }
--- src/main/java/by/itacademy/valerymichailuk/buka/user/User.java
+++ src/main/java/by/itacademy/valerymichailuk/buka/user/User.java
@@ -7,7 +7,7 @@ public class User {
     public static final String NEW_USER_PASSWORD = "8GJYaJ";
     public static final String USER_NAME = "valery.mihkailuk@gmail.com";
     public static final String USER_PASSWORD = "83528352";
-    public static final String INVALID_USER_NAME = "agent.english@gmail.com";
+    public static final String INVALID_USER_NAME = "agents.paddington@gmail.com";
     public static final String INVALID_USER_PASSWORD = "876gfff";
     public static final String ERROR_EMPTY_PASSWORD_TEXT = "Не введен пароль";
     public static final String ERROR_EMPTY_LOGIN_TEXT = "Не введено имя пользователя";
